1901dcfb4951d45d5ceeef4855c2898845f08d52
KholishN <105257977+KholishN@users.noreply.github.com>
2022-09-23 18:18:25 +0700

    Update api.jsx

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://employee-istidata.herokuapp.com/api/v1/employees",
+  baseURL: "https://employee-istidata.herokuapp.com/api/v1/",
 });
